368bc799859fcfab0bc6e9d59090a05df29b2903
shoheiyokoyama <shohei.yok0602@gmail.com>
2017-06-10 17:09:08 +0900

    Create project

---
+++ Example/Pods/Target Support Files/Gemini/Gemini-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Gemini : NSObject
+@end
+@implementation PodsDummy_Gemini
+@end
---
+++ Example/Pods/Target Support Files/Pods-Gemini_Example/Pods-Gemini_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Gemini_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Gemini_Example
+@end
